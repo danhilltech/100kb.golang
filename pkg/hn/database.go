d48cb347f6e912a1c22d6f8035a0a7d24d4e3181
Dan Hill <dan@townsendhill.com>
2024-01-28 13:46:49 -0500

    fix: fixes

--- pkg/hn/database.go
+++ pkg/hn/database.go
@@ -49,26 +49,3 @@ func (engine *Engine) getExistingIDs(txchan *sql.Tx) ([]int, error) {
 	}
 	return ids, nil
 }
-
-func (engine *Engine) GetExistingIDs(txchan *sql.Tx) ([]int, error) {
-	res, err := txchan.Query("SELECT id FROM hacker_news")
-	if err != nil {
-		return nil, err
-	}
-	defer res.Close()
-
-	var ids []int
-
-	for res.Next() {
-		var id int
-		err = res.Scan(&id)
-		if err != nil {
-			return nil, err
-		}
-		ids = append(ids, id)
-	}
-	if err := res.Err(); err != nil {
-		return nil, err
-	}
-	return ids, nil
-}
