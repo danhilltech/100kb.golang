bb26254b8e622ef5ef8b392874ac25ffa4c118c7
Dan Hill <dan@townsendhill.com>
2024-02-11 13:01:16 -0500

    fix: error wrapping

--- pkg/parsing/meta.go
+++ pkg/parsing/meta.go
@@ -2,6 +2,7 @@ package parsing
 
 import (
 	_ "embed"
+	"fmt"
 	"net/url"
 	"strings"
 
@@ -21,20 +22,22 @@ func (engine *Engine) IdentifyInternalPages(z *html.Node, baseUrl string) (bool,
 
 	baseUrlP, err := url.Parse(baseUrl)
 	if err != nil {
-		return false, false, false, err
+		return false, false, false, fmt.Errorf("could not parse url %s %w", baseUrl, err)
 	}
 
 	for _, ic := range internalLinkTags {
 		sel, err := cascadia.Parse(ic)
 		if err != nil {
-			return false, false, false, err
+			return false, false, false, fmt.Errorf("could not create cascadia filter %w", err)
 		}
 		for _, a := range cascadia.QueryAll(z, sel) {
 			for _, attr := range a.Attr {
 				if attr.Key == "href" && (strings.HasPrefix(attr.Val, "http") || strings.HasPrefix(attr.Val, "/")) {
-					uP, err := url.Parse(attr.Val)
+					valUrl := strings.TrimSpace(strings.ToLower(attr.Val))
+
+					uP, err := url.Parse(valUrl)
 					if err != nil {
-						return false, false, false, err
+						return false, false, false, fmt.Errorf("could not parse url '%s' %w", valUrl, err)
 					}
 					resolv := baseUrlP.ResolveReference(uP)
 
